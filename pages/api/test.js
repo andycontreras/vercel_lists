a3d6c9f0ae80ad0ede08cb0a44b893f0aadca2dd
Andrés Contreras <andresconvar@gmail.com>
2025-03-28 00:22:03 -0300

    se mueve a next

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,8 +1,10 @@
+import { test } from './index.js';
+
 export default function andy(req, res) {
   res.status(200).json({
     body: req.body,
     query: req.query,
     cookies: req.cookies,
-    index: true,
+    test: test(),
   });
 }
